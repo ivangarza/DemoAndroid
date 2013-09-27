835252a496247da2661ba3e0066ee06024a9cfaf
Ivan Garza <usuario@MacBook-de-Propietario.local>
2013-09-27 04:07:23 -0500

    Ajustes

--- TestYapp/src/com/test/testyapp/Model.java
+++ TestYapp/src/com/test/testyapp/Model.java
@@ -9,12 +9,12 @@ public class Model {
     public static void LoadModel() {
 
         Items = new ArrayList<Item>();
-        Items.add(new Item(1, "foto_espectaculos.png", "Espect‡culos","Shakira muestra la primera foto de Milan"));
+        Items.add(new Item(1, "foto_espectaculos.png", "Espectaculos","Shakira muestra la primera foto de Milan"));
         Items.add(new Item(2, "foto_hoy.png", "Lo de Hoy","Secuestra a 3 personas en la comunidad"));
-        Items.add(new Item(3, "foto_horoscopos.png", "Hor—scopos","Aries 5 Febrero..."));
+        Items.add(new Item(3, "foto_horoscopos.png", "Horoscopos","Aries 5 Febrero..."));
         Items.add(new Item(4, "foto_cine.png", "Cine","Estreno de Hansel y Gretel."));
-        Items.add(new Item(5, "foto_clima.png", "Clima","Monterrey: Max 23¼ Min 9¼"));
-        Items.add(new Item(6, "foto_deportes.png", "Deportes","Prepara Messi su renovaci—n con el barca"));
+        Items.add(new Item(5, "foto_clima.png", "Clima","Monterrey: Max 23 Min 9"));
+        Items.add(new Item(6, "foto_deportes.png", "Deportes","Prepara Messi su renovacion con el barca"));
         //Items.add(new Item(7, "ic_action_stamp.png", "Stamp"));
         
         /*Items.add(new Item(1, "ic_action_alarm_2.png", "Alarm"));
